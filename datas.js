e9e37f4cb3a0a09a61b81f616889d470b0bb6b67
liyongleihf2006 <573044965@qq.com>
2016-12-30 16:02:35 +0800

    that's ok,oh yeah

---
+++ datas.js
@@ -0,0 +1,477 @@
+/**
+ * Created by LiYonglei on 2016/12/30.
+ */
+var parentIdDatas = [{
+    "disporder": 0,
+    "f_parent_id": 0,
+    "id": 288,
+    "name": "企业商情",
+    "optime": "2016-08-25 08:56:53",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 288,
+    "id": 289,
+    "name": "上市企业",
+    "optime": "2016-08-25 08:57:11",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 186,
+    "id": 208,
+    "name": "文库",
+    "optime": "2016-04-11 15:56:55",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 208,
+    "id": 288,
+    "name": "288",
+    "optime": "2016-04-11 15:56:55",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 208,
+    "id": 289,
+    "name": "289",
+    "optime": "2016-04-11 15:56:55",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 248,
+    "name": "迷药",
+    "optime": "2016-08-03 17:35:19",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 142,
+    "name": "游戏广告",
+    "optime": "2014-11-02 15:43:55",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 0,
+    "id": 162,
+    "name": "问答",
+    "optime": "2014-11-03 18:54:07",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 162,
+    "id": 163,
+    "name": "问答",
+    "optime": "2014-11-03 18:55:23",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 164,
+    "name": "婚恋信息",
+    "optime": "2014-11-03 18:58:30",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 0,
+    "id": 82,
+    "name": "违法信息",
+    "optime": "2014-10-24 21:12:14",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 84,
+    "name": "发票",
+    "optime": "2014-10-24 21:14:57",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 86,
+    "name": "涉赌",
+    "optime": "2014-10-24 21:15:16",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 88,
+    "name": "涉毒",
+    "optime": "2014-10-24 21:15:52",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 0,
+    "id": 90,
+    "name": "商情广告",
+    "optime": "2014-10-24 21:16:29",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 92,
+    "name": "医疗器械",
+    "optime": "2014-10-24 21:17:01",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 94,
+    "name": "医院推广",
+    "optime": "2014-10-24 21:17:21",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 96,
+    "name": "装修卖房",
+    "optime": "2014-10-24 21:17:43",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 100,
+    "name": "教育培训",
+    "optime": "2014-10-24 21:18:21",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 102,
+    "name": "旅游推广",
+    "optime": "2014-10-24 21:18:44",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 103,
+    "name": "其它",
+    "optime": "2014-10-24 21:25:21",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 83,
+    "name": "办证",
+    "optime": "2014-10-24 21:13:10",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 87,
+    "name": "涉枪涉爆",
+    "optime": "2014-10-24 21:15:29",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 89,
+    "name": "代考",
+    "optime": "2014-10-24 21:16:09",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 91,
+    "name": " 商业信息",
+    "optime": "2014-10-24 21:16:42",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 93,
+    "name": "公司介绍",
+    "optime": "2014-10-24 21:17:11",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 95,
+    "name": "团购信息",
+    "optime": "2014-10-24 21:17:34",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 186,
+    "id": 187,
+    "name": "宗教",
+    "optime": "2015-08-31 15:05:59",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 187,
+    "id": 1871,
+    "name": "1871",
+    "optime": "2015-08-31 15:05:59",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 187,
+    "id": 1872,
+    "name": "1872",
+    "optime": "2015-08-31 15:05:59",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 1872,
+    "id": 18721,
+    "name": "18721",
+    "optime": "2015-08-31 15:05:59",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 99,
+    "name": "招投标信息",
+    "optime": "2014-10-24 21:18:11",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 101,
+    "name": "招租求租",
+    "optime": "2014-10-24 21:18:34",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 122,
+    "name": "招聘信息",
+    "optime": "2014-10-28 10:05:19",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 165,
+    "name": "色情信息",
+    "optime": "2014-11-05 11:33:15",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 182,
+    "name": "涉赌2",
+    "optime": "2015-07-13 10:07:16",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 183,
+    "name": "色情信息2",
+    "optime": "2015-07-13 10:48:42",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 184,
+    "name": "色情信息3",
+    "optime": "2015-07-13 10:50:11",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 82,
+    "id": 185,
+    "name": "色情信息4",
+    "optime": "2015-07-13 10:51:19",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 0,
+    "id": 186,
+    "name": "其他",
+    "optime": "2015-08-31 15:05:39",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 188,
+    "name": "招投标信息",
+    "optime": "2015-12-28 18:16:01",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 308,
+    "name": "影视剧情",
+    "optime": "2016-11-09 14:25:19",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 228,
+    "name": "违法商品",
+    "optime": "2016-07-19 11:13:28",
+    "status": 0
+}, {
+    "disporder": 0,
+    "f_parent_id": 90,
+    "id": 229,
+    "name": "成人用品",
+    "optime": "2016-07-19 11:42:48",
+    "status": 0
+}, {"disporder": 0, "f_parent_id": 90, "id": 268, "name": "保健品", "optime": "2016-08-05 16:34:42", "status": 0}];
+var cascadeDatas = [{
+    "disporder": 0,
+    "id": 186,
+    "name": "其他",
+    "optime": "2015-08-31 15:05:39",
+    "status": 0,
+    "children": [{
+        "disporder": 0,
+        "id": 187,
+        "name": "宗教",
+        "optime": "2015-08-31 15:05:59",
+        "status": 0,
+        "children": [{
+            "disporder": 0,
+            "id": 1872,
+            "name": "1872",
+            "optime": "2015-08-31 15:05:59",
+            "status": 0,
+            "children": [{
+                "disporder": 0,
+                "id": 18721,
+                "name": "18721",
+                "optime": "2015-08-31 15:05:59",
+                "status": 0
+            }]
+        }, {"disporder": 0, "id": 1871, "name": "1871", "optime": "2015-08-31 15:05:59", "status": 0}]
+    }, {
+        "disporder": 0,
+        "id": 208,
+        "name": "文库",
+        "optime": "2016-04-11 15:56:55",
+        "status": 0,
+        "children": [{
+            "disporder": 0,
+            "id": 289,
+            "name": "289",
+            "optime": "2016-04-11 15:56:55",
+            "status": 0
+        }, {
+            "disporder": 0,
+            "id": 288,
+            "name": "288",
+            "optime": "2016-04-11 15:56:55",
+            "status": 0,
+            "children": [{"disporder": 0, "id": 289, "name": "上市企业", "optime": "2016-08-25 08:57:11", "status": 0}]
+        }]
+    }]
+}, {
+    "disporder": 0,
+    "id": 90,
+    "name": "商情广告",
+    "optime": "2014-10-24 21:16:29",
+    "status": 0,
+    "children": [{
+        "disporder": 0,
+        "id": 268,
+        "name": "保健品",
+        "optime": "2016-08-05 16:34:42",
+        "status": 0
+    }, {"disporder": 0, "id": 229, "name": "成人用品", "optime": "2016-07-19 11:42:48", "status": 0}, {
+        "disporder": 0,
+        "id": 228,
+        "name": "违法商品",
+        "optime": "2016-07-19 11:13:28",
+        "status": 0
+    }, {"disporder": 0, "id": 308, "name": "影视剧情", "optime": "2016-11-09 14:25:19", "status": 0}, {
+        "disporder": 0,
+        "id": 188,
+        "name": "招投标信息",
+        "optime": "2015-12-28 18:16:01",
+        "status": 0
+    }, {"disporder": 0, "id": 122, "name": "招聘信息", "optime": "2014-10-28 10:05:19", "status": 0}, {
+        "disporder": 0,
+        "id": 101,
+        "name": "招租求租",
+        "optime": "2014-10-24 21:18:34",
+        "status": 0
+    }, {"disporder": 0, "id": 99, "name": "招投标信息", "optime": "2014-10-24 21:18:11", "status": 0}, {
+        "disporder": 0,
+        "id": 95,
+        "name": "团购信息",
+        "optime": "2014-10-24 21:17:34",
+        "status": 0
+    }, {"disporder": 0, "id": 93, "name": "公司介绍", "optime": "2014-10-24 21:17:11", "status": 0}, {
+        "disporder": 0,
+        "id": 91,
+        "name": " 商业信息",
+        "optime": "2014-10-24 21:16:42",
+        "status": 0
+    }, {"disporder": 0, "id": 102, "name": "旅游推广", "optime": "2014-10-24 21:18:44", "status": 0}, {
+        "disporder": 0,
+        "id": 100,
+        "name": "教育培训",
+        "optime": "2014-10-24 21:18:21",
+        "status": 0
+    }, {"disporder": 0, "id": 96, "name": "装修卖房", "optime": "2014-10-24 21:17:43", "status": 0}, {
+        "disporder": 0,
+        "id": 94,
+        "name": "医院推广",
+        "optime": "2014-10-24 21:17:21",
+        "status": 0
+    }, {"disporder": 0, "id": 92, "name": "医疗器械", "optime": "2014-10-24 21:17:01", "status": 0}, {
+        "disporder": 0,
+        "id": 164,
+        "name": "婚恋信息",
+        "optime": "2014-11-03 18:58:30",
+        "status": 0
+    }, {"disporder": 0, "id": 142, "name": "游戏广告", "optime": "2014-11-02 15:43:55", "status": 0}]
+}, {
+    "disporder": 0,
+    "id": 82,
+    "name": "违法信息",
+    "optime": "2014-10-24 21:12:14",
+    "status": 0,
+    "children": [{
+        "disporder": 0,
+        "id": 185,
+        "name": "色情信息4",
+        "optime": "2015-07-13 10:51:19",
+        "status": 0
+    }, {"disporder": 0, "id": 184, "name": "色情信息3", "optime": "2015-07-13 10:50:11", "status": 0}, {
+        "disporder": 0,
+        "id": 183,
+        "name": "色情信息2",
+        "optime": "2015-07-13 10:48:42",
+        "status": 0
+    }, {"disporder": 0, "id": 182, "name": "涉赌2", "optime": "2015-07-13 10:07:16", "status": 0}, {
+        "disporder": 0,
+        "id": 165,
+        "name": "色情信息",
+        "optime": "2014-11-05 11:33:15",
+        "status": 0
+    }, {"disporder": 0, "id": 89, "name": "代考", "optime": "2014-10-24 21:16:09", "status": 0}, {
+        "disporder": 0,
+        "id": 87,
+        "name": "涉枪涉爆",
+        "optime": "2014-10-24 21:15:29",
+        "status": 0
+    }, {"disporder": 0, "id": 83, "name": "办证", "optime": "2014-10-24 21:13:10", "status": 0}, {
+        "disporder": 0,
+        "id": 103,
+        "name": "其它",
+        "optime": "2014-10-24 21:25:21",
+        "status": 0
+    }, {"disporder": 0, "id": 88, "name": "涉毒", "optime": "2014-10-24 21:15:52", "status": 0}, {
+        "disporder": 0,
+        "id": 86,
+        "name": "涉赌",
+        "optime": "2014-10-24 21:15:16",
+        "status": 0
+    }, {"disporder": 0, "id": 84, "name": "发票", "optime": "2014-10-24 21:14:57", "status": 0}, {
+        "disporder": 0,
+        "id": 248,
+        "name": "迷药",
+        "optime": "2016-08-03 17:35:19",
+        "status": 0
+    }]
+}, {
+    "disporder": 0,
+    "id": 162,
+    "name": "问答",
+    "optime": "2014-11-03 18:54:07",
+    "status": 0,
+    "children": [{"disporder": 0, "id": 163, "name": "问答", "optime": "2014-11-03 18:55:23", "status": 0}]
+}, {"disporder": 0, "id": 288, "name": "企业商情", "optime": "2016-08-25 08:56:53", "status": 0}];
---
+++ parentIdCascadeTransform.js
@@ -0,0 +1,67 @@
+/**
+ * Created by LiYonglei on 2016/12/30.
+ */
+/*
+ * 由parentId格式的数据向cascade格式的数据转化
+ * params:
+ *       datas:要转化的数据
+ *       id:每一项的索引键名,default:"id"
+ *       pid:每一项的父索引键名,default:"pid"
+ *       rootId:根元素的索引值,default:0,其实就是决定哪些元素放到最上层的数组中去
+ *       childrenKey:指向子数组的键名,default:"children"
+ * */
+function parentIdToCascade(datas, id, pid, rootId, childrenKey) {
+    datas = JSON.parse(JSON.stringify(datas));
+    id = id || "id";
+    pid = pid || "pid";
+    rootId = rootId || 0;
+    childrenKey = childrenKey || "children";
+    return transform();
+    function transform(parentItem) {
+        var children = datas.reduceRight(function (target, item, idx) {
+            if (parentItem ? item[pid] == parentItem[id] : item[pid] == rootId) {
+                [].push.apply(target, datas.splice(idx, 1));
+            };
+            return target;
+        }, []);
+        children.forEach(function (item) {
+            transform(item);
+        });
+        if (!parentItem) {
+            return children;
+        } else if (children.length) {
+            parentItem[childrenKey] = children;
+        }
+    };
+};
+/*
+ * 由cascade格式的数据向parentId格式的数据转化
+ * params:
+ *       datas:要转化的数据
+ *       id:每一项的索引键名,default:"id"
+ *       pid:指向父元素的索引键名,default:"pid"
+ *       rootId:根元素的索引值,default:0,其实就是最顶层元素的父索引的值
+ *       childrenKey:指向子数组的键名,default:"children"
+ * */
+function cascadeToParentId(datas, id, pid, rootId, childrenKey) {
+    datas = JSON.parse(JSON.stringify(datas));
+    id = id || "id";
+    pid = pid || "pid";
+    rootId = rootId || 0;
+    childrenKey = childrenKey || "children";
+    var arr = [];
+    datas.forEach(function(item){
+        item[pid]=rootId;
+        arr.push(item);
+        transform(item);
+    });
+    return arr;
+    function transform(parentItem) {
+        (parentItem[childrenKey] || []).forEach(function (item) {
+            item[pid] = parentItem[id];
+            arr.push(item);
+            transform(item);
+        });
+        delete parentItem[childrenKey];
+    };
+};
